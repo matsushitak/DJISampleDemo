6bddc5fac076362b9c13052fa19640d0db84795a
matsushita <ad.special008@gmail.com>
2019-01-30 22:21:24 +0900

    exvironment: first commit

---
+++ DJISampleDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DJISampleDemo
+//
+//  Created by 松下航平 on 2019/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'DJISampleDemo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for DJISampleDemo
+  pod 'DJIWidget', '~> 1.2'
+
+end
