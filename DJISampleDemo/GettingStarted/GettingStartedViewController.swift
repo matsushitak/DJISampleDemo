cd375a361865139691ce694e48147a7f96e0891a
matsushita <ad.special008@gmail.com>
2019-03-03 20:43:51 +0900

    #3 Activating GettingStartedViewController

--- DJISampleDemo/GettingStarted/GettingStartedViewController.swift
+++ DJISampleDemo/GettingStarted/GettingStartedViewController.swift
@@ -6,25 +6,30 @@
 //
 
 import UIKit
+import DJISDK
 import DJIUXSDK
 
+/// Getting Started with UX SDK
 class GettingStartedViewController: DUXDefaultLayoutViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        DJISDKManager.registerApp(with: self)
     }
-    */
+}
 
+extension GettingStartedViewController: DJISDKManagerDelegate {
+
+    func appRegisteredWithError(_ error: Error?) {
+        if let error = error {
+            print("Register App Failured : " + error.localizedDescription)
+        } else {
+            print("Register App Successed")
+            DJISDKManager.startConnectionToProduct()
+        }
+    }
 }
